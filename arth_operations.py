5dc53eb4ffbc6d4e9da758902623be68ac9ef620
madhu9515 <139426514+madhu9515@users.noreply.github.com>
2023-11-14 22:31:07 +0530

    Add files via upload

---
+++ arth_operations.py
@@ -0,0 +1,9 @@
+x=20
+y=30
+print("arethemetic operators")
+print(f'sum of {x} and {y} is {x+y}')
+print(f'substraction of {x} and {y} is {x-y}')
+print(f'multiplication of {x} and {y} is {x*y}')
+print(f'division of {x} and {y} is {y/x}')
+print(f'modulo of {x} and {y} is {x%y}')
+print(f'power of {x} and {y} is {x**y}')
