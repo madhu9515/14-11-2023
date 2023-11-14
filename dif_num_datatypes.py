26d940aa663166e07c15f0847dd57569741e0389
madhu9515 <139426514+madhu9515@users.noreply.github.com>
2023-11-14 22:20:37 +0530

    Add files via upload

---
+++ dif_num_datatypes.py
@@ -0,0 +1,39 @@
+a=10
+b=1.2
+c=1j
+d=8.0
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print()
+print("prethemetic operators")
+print(a+b)
+print(a-c)
+print(a%b)
+print(a//b)
+print(a*b)
+print(a**b)
+print(a/b)
+print()
+print("comparison operator")
+print(a==b)
+print(a>b)
+print(a<b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print()
+
+print("identity operator")
+print(a is b)
+print(a is not b)
+print()
+print("logical operator")
+print(a and b)
+print(a or b)
+print(not a)
+print(a or b)
+print(not b)
+
+
